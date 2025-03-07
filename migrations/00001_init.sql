5909067161b02507a3a0bdad6e60708e653e6a0c
Justin Trahan <justin.m.trahan@outlook.com>
2025-03-07 21:34:02 +0000

    added basic tables

--- migrations/00001_init.sql
+++ migrations/00001_init.sql
@@ -2,18 +2,20 @@
 
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS s3_buckets (
-  id uuid PRIMARY KEY DEFAULT gen_random_uuid()
-  provider_name TEXT NOT NULL
-  ui_url TEXT NULL
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  provider_name TEXT NOT NULL,
+  ui_url TEXT NULL,
   admin_url TEXT NULL
 )
 -- +goose StatementEnd
 
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS squared_shares(
-  id uuid PRIMARY KEY DEFAULT gen_random_uuid()
-  s3_bucket_id uuid NULL
-  local_path TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS squared_shares (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  s3_bucket_id uuid REFERENCES s3_buckets (id),
+  local_path TEXT NOT NULL,
+  smb_path TEXT NULL,
+  quota_size TEXT NULL
 )
 -- +goose StatementEnd
 
