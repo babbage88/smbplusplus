e84fa56b802403511e69fa098052375028d42779
Justin Trahan <justin.m.trahan@outlook.com>
2025-03-07 21:22:01 +0000

    working on migration 1

--- migrations/00001_init.sql
+++ migrations/00001_init.sql
@@ -1,9 +1,19 @@
 -- +goose Up
+
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS s3_buckets (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid()
+  provider_name TEXT NOT NULL
+  ui_url TEXT NULL
+  admin_url TEXT NULL
+)
+-- +goose StatementEnd
+
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS squared_shares(
   id uuid PRIMARY KEY DEFAULT gen_random_uuid()
+  s3_bucket_id uuid NULL
   local_path TEXT NOT NULL
-  s3_bucket_id uuiD NULL
 )
 -- +goose StatementEnd
 
