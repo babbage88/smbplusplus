f569b080823dda3f0c43c66e86077c280347ac0b
jtrahan <justin@trahan.dev>
2025-03-08 07:51:06 -0600

    Add default admin user and roles

--- migrations/00003_create_devuser.sql
+++ migrations/00003_create_devuser.sql
@@ -6,10 +6,12 @@
  (id, username, "password", email, "role", created_at, last_modified)
  VALUES(gen_random_uuid(), 'devuser', '${DEV_APP_USER_PW}', 'devuser@test.trahan.dev', 'admin', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 -- +goose envsub off
+-- +goose StatementEnd
 
 -- +goose StatementBegin
-
 -- Generate the UUID for the Admin role
+-- Insert permissions if they do not exist
+-- Insert role-permission mappings
 WITH admin_role AS (
     INSERT INTO public.user_roles (id, role_name, role_description)
     VALUES (gen_random_uuid(), 'Admin', 'Administrator role with all permissions')
@@ -17,7 +19,7 @@ WITH admin_role AS (
     RETURNING id
 ),
 permissions AS (
-    -- Insert permissions if they do not exist
+
     INSERT INTO public.app_permissions (permission_name, permission_description)
     VALUES
         ('CreateUser', 'Permission to create users'),
@@ -26,7 +28,6 @@ permissions AS (
     ON CONFLICT (permission_name) DO NOTHING
     RETURNING id, permission_name
 )
--- Insert role-permission mappings
 INSERT INTO public.role_permission_mapping (role_id, permission_id)
 SELECT COALESCE((SELECT id FROM admin_role), (SELECT id FROM public.user_roles WHERE role_name = 'Admin')),
        p.id
@@ -37,5 +38,5 @@ WHERE p.permission_name IN ('CreateUser', 'AlterUser', 'CreateDatabase');
 
 -- +goose Down
 -- +goose StatementBegin
-DELETE FROM public.users WHERE "username" = 'devuser'
+DELETE FROM public.users WHERE "username" = 'devuser';
 -- +goose StatementEnd
